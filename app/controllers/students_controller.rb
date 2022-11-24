0b66682a6c642438413cda2a31ec22224a459c49
Ashley <ashleynjeri7@gmail.com>
2022-11-24 14:34:48 +0300

    Passed tests

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,15 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+# Returns student by id
+  def first_student
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  # def query
+  #   student = Student.find(params[first_name: 'Vanessa'])
+  #   render json: student
+  # end
+
 end
